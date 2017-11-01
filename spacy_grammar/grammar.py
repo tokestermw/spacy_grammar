297dd9374bff6b2b00b845499f73da797545e34b
Motoki Wu <tokestermw@gmail.com>
2017-11-01 10:07:27 -0700

    Adds option to match on a list of token attributes.
    For example, the following will match 'as' and 'follow' for the
    'lower' attribute for a spaCy token.
    
    - LOWER: as
    - LOWER: follow
    
    Also removed some redundancies in the code.

--- spacy_grammar/grammar.py
+++ spacy_grammar/grammar.py
@@ -1,48 +1,28 @@
-from spacy.tokens import Doc, Span, Token
-from spacy.matcher import PhraseMatcher
+from itertools import chain
 
-from .utils import load_grammar_file
-
-PREFIX = 'g_'
-
-
-# TODO: put into class (to keep descriptions, etc.)?
-def parse_grammar_matchers(nlp):
-    grammar_file = load_grammar_file(nlp.lang)
-    matchers = []
-    for category, rule in grammar_file.items():
-        matcher = PhraseMatcher(nlp.vocab)
-        for name, attributes in rule.items():
-            grammar_patterns = map(nlp, attributes['patterns'])
-            matcher.add(name, None, *grammar_patterns)
-            Token.set_extension(PREFIX + name, default=False)
-        matchers.append(matcher)
-    return matchers
+from .pattern import parse_grammar_matcher, PREFIX
 
 
 class Grammar(object):
 
-    def __init__(self, nlp, merge_spans=False):
+    def __init__(self, nlp):
 
-        self.merge_spans = merge_spans
-        self.matchers = parse_grammar_matchers(nlp)
+        self.matcher, self.phrase_matcher = parse_grammar_matcher(nlp)
 
     def __call__(self, doc):
 
-        matches = []
-        for matcher in self.matchers:
-            matches.extend(matcher(doc))
+        matches = self.matcher(doc)
+        phrase_matches = self.phrase_matcher(doc)
 
-        spans = []  # keep spans here to merge them later
-        for ent_id, start, end in matches:
+        # TODO: remove duplicates
+        spans = []
+        for ent_id, start, end in chain(matches, phrase_matches):
             ent_name = doc.vocab[ent_id].text
             span = doc[start : end]
             for token in span:
                 token._.set(PREFIX + ent_name, True)
             spans.append(span)
-        if self.merge_spans:
-            for span in spans:
-                span.merge()
+
         return doc
 
 
